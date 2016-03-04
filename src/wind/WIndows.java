f6dae0bc9d485e167dc35732a2cb50038dba5e36
Basalai Anton <antonbasalai@mail.ru>
2016-03-04 16:47:37 +0300

    Add files 3

--- src/wind/WIndows.java
+++ src/wind/WIndows.java
@@ -1,34 +1,37 @@
 package wind;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-/**
- * Created by aantonbasalai on 26.02.2016.
- */
 public class WIndows
 {
-
     public WIndows()
     {
-       // super ("Лабораторная №1");
         JFrame WIndows = new JFrame(" Лабораторная работа №1");
         JPanel wind=new JPanel(new FlowLayout());
         JButton addInComboBox = new JButton("1");
-       // button1.setLocation(20,20);
+        addInComboBox.setLocation(20,20);
         wind.add(addInComboBox);
         WIndows.add(wind);
-        JTextField textField = new JTextField();
+        JTabbedPane tb =new JTabbedPane();
+        JTextField textField = new JTextField("",20);
+        textField.setSize(20,40);
         wind.add(textField);
-        JComboBox<String> comboBox = new JComboBox<String>();
-        wind.add(comboBox);
+        DefaultComboBoxModel<String> model = new DefaultComboBoxModel<String>();
+        JComboBox<String> textList = new JComboBox<String>(model);
+        wind.add(textList);
         addInComboBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String text = textField.getText();
-                comboBox.addItem(text);
+                if (!text.isEmpty()) {
+                    if (model.getIndexOf(text)==-1){
+                        model.addElement(text);
+                    }
+                    else JOptionPane.showMessageDialog(WIndows,"Повторяющийся текст");
+                }
+                else JOptionPane.showMessageDialog(WIndows,"Введите текст");
             }
         });
         WIndows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
